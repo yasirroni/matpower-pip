aa4a355f6a89ebbf30c497b4301f6df279018410
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2022-06-24 10:09:10 +0700

    add tests

--- tests/test_core_functionality.py
+++ tests/test_core_functionality.py
@@ -1,13 +1,40 @@
-# placeholder, temporary test
+import numpy as np
+import pytest
 
-# test import
 import matpower
+from matpower import __MATPOWER_VERSION__, __MATPOWERPIP_VERSION__
 
-# test print version
-matpower.__version__
 
-# test path
-matpower.__path__[0] == matpower.path_matpower
+"""Test using pytest
 
-# test start instante
-m = matpower.start_instance(engine='lala')
+    pytest -rA -c pyproject.toml --cov-report term --cov=matpowerpip tests/
+"""
+
+class TestCoreFunctionality:
+    @classmethod
+    def setup_method(cls):
+        pass
+
+    def test_version(self):
+        print(f"matpower.__version__: {matpower.__version__}")
+        assert len(matpower.__version__.split(".")) == 6
+        assert matpower.__version__.startswith(__MATPOWER_VERSION__) == True
+        assert matpower.__version__.endswith(__MATPOWERPIP_VERSION__) == True
+
+    def test_path(self):
+        assert matpower.__path__[0] == matpower.path_matpower
+
+    def test_instance_octave(self):
+        m = matpower.start_instance(engine='octave')
+        mpc = m.loadcase('case9')
+        case9_gencost_val = np.array(
+            [[2.000e+00, 1.500e+03, 0.000e+00, 3.000e+00, 1.100e-01, 5.000e+00, 1.500e+02],
+             [2.000e+00, 2.000e+03, 0.000e+00, 3.000e+00, 8.500e-02, 1.200e+00, 6.000e+02],
+             [2.000e+00, 3.000e+03, 0.000e+00, 3.000e+00, 1.225e-01, 1.000e+00, 3.350e+02]]
+        )
+        assert np.allclose(mpc.gencost, case9_gencost_val) == True
+
+        # NOTE: install_matpowerwill fail if matpower already installed
+        # TODO: Check path is in engine path or not
+        # matpower.install_matpower(path_matpower=matpower.path_matpower, session=m, verbose=False)
+        # matpower.uninstall_matpower(path_matpower=matpower.path_matpower, session=m, verbose=False)
