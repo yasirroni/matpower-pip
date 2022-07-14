4804e3b3306f1de98c4815e1191cde8a14be1846
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2022-07-14 16:08:13 +0700

    bump new version

--- matpowerpip/__init__.py
+++ matpowerpip/__init__.py
@@ -116,7 +116,7 @@ path_matpower = PATH_MATPOWER
 path_matpower_cases = os.path.join(path_matpower, 'data')
 cases = os.listdir(path_matpower_cases)
 
-__MATPOWERPIP_VERSION__ = "2.1.4a0"
+__MATPOWERPIP_VERSION__ = "2.1.4"
 
 try:
     current_path = os.path.abspath(os.path.dirname(__file__))
